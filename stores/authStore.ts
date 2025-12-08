eef9e4d9c1b7df32aeb1f99dbb7496641900b51f
CarlosAtienza <atienzacarlos72@gmail.com>
2025-12-08 15:30:29 -0600

    Merged userStore into authStore and rm userStore

--- stores/authStore.ts
+++ stores/authStore.ts
@@ -1,10 +1,13 @@
+import { UserResponseDTO } from "@/types";
 import * as SecureStore from "expo-secure-store";
 import { create } from "zustand";
 
 interface AuthState {
     token: string | null;
-    
     userId: number | null;
+    //Current User profile data
+    user: UserResponseDTO | null;
+    setUser: (user: UserResponseDTO) => void;
     setToken: (token: string) => void;
 
     logout: () => Promise<void>;
@@ -14,9 +17,10 @@ interface AuthState {
 
 export const useAuthStore = create<AuthState>((set) => ({
     token: null,
-
+    user: null,
     userId: null,
     setToken: (token: string ) => set({ token }),
+    setUser: (user: UserResponseDTO) => ({ user }),
    
 
     logout: async () => {
